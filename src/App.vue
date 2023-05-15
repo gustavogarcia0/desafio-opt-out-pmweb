12f8a8467a9ebb6f48d5074eec697bfa637c8fa0
gustavo garcia <gustavogarcia.profissional@gmail.com>
2023-05-15 14:12:40 -0300

    desafio completo

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <containerOptOut />
+</template>
+
+<script>
+import containerOptOut from "./components/containersOptOut.vue";
+
+export default {
+  name: "App",
+  components: {
+    containerOptOut,
+  },
+};
+</script>
